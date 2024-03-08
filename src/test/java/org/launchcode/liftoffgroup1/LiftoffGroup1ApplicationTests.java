48d2bacc6d719e827c554ab5f7c2386c0a328bed
ethandabbs2 <ethandabbs2@yahoo.com>
2024-03-07 19:57:49 -0600

    commit

---
+++ src/test/java/org/launchcode/liftoffgroup1/LiftoffGroup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoffgroup1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffGroup1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
