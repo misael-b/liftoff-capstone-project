6ff3e9f61d96258663d093d0e8dcf22b24683d85
ethandabbs2 <ethandabbs2@yahoo.com>
2024-03-28 14:34:15 -0500

    small update

--- backend/src/main/java/org/launchcode/liftoffgroup1/controllers/ProductController.java
+++ backend/src/main/java/org/launchcode/liftoffgroup1/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package org.launchcode.liftoffgroup1.controllers;
 
+import org.launchcode.liftoffgroup1.model.Product;
 import org.launchcode.liftoffgroup1.model.data.ProductRepository;
 import org.launchcode.liftoffgroup1.model.data.ShoppingCartRepository;
 import org.launchcode.liftoffgroup1.model.data.UserRepository;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,12 +26,15 @@ public class ProductController {
 
     @GetMapping("create")
     public String displayCreatePostForm(Model model) {
+
+        model.addAttribute("product", new Product());
         return "post/create-post";
     }
 
     @PostMapping("create")
-    public String processCreatePostForm() {
-        return "";
+    public String processCreatePostForm(@ModelAttribute Product product) {
+        productRepository.save(product);
+        return "/index";
     }
 
     @GetMapping("delete")
