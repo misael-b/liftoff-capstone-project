72105e336488ec974808ea9851726b08a0da4f44
mbarcenas <misael.barcenas@yahoo.com>
2024-03-31 16:22:43 -0500

    user crud

--- frontend/frontend/src/app/components/user-home-page/page.jsx
+++ frontend/frontend/src/app/components/user-home-page/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import GetFromUsername from './getUser'
+import Profile from './Profile'
 
 
 const page = () => {
     return (
         <>
-           <GetFromUsername/>
+            <GetFromUsername />
+            <Profile/>
         </>
     )
 }
