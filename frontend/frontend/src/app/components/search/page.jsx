ce26b38de1f92e7c1e9e75574730ec8469a15bd1
mbarcenas <misael.barcenas@yahoo.com>
2024-04-01 14:34:08 -0500

    search bar

---
+++ frontend/frontend/src/app/components/search/page.jsx
@@ -0,0 +1,9 @@
+import Search from "./search";
+import Search2 from "./search2";
+
+export default function SearchPage(){
+    return (<>
+        {/* <Search /> */}
+        <Search2 />
+    </>)
+}
