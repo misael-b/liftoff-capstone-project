d90f2aeb03425e7bfd067e749512187bf122e596
Kyle Banderman <kyle.banderman4675@gmail.com>
2024-04-02 20:18:52 -0500

    almost finished navbar

--- frontend/frontend/src/app/components/NavBar.jsx
+++ frontend/frontend/src/app/components/NavBar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, {useState} from 'react'
-import css from './NavBar.css'
-import {Link} from "react-router-dom";
+import css from '../../app/globals.css'
+import Link from "next/link";
 //all of these links can be changed to whatever we need to later
 const NavBar = () => {
   const [search, setSearch] = useState({search: ''})
@@ -17,16 +17,17 @@ const NavBar = () => {
   const handleChange = (event) => {
     const {name, value} = event.target;
     setSearch(prevSearch => ({ ...prevSearch, [name]: value}));
-};
+  };
 
   return (
     <> 
-    <div class="navbar">
+    <div className="navbar">
         <Link href='http://localhost:3000/profile'><img src="https://www.dropbox.com/scl/fi/p3jwfys2i99m6ypluwfww/Profile.png?rlkey=r3lppn8mj0msj5ventzkjya83&raw=1" alt="Profile" id="profile"/></Link>
         <Link href='http://localhost:3000/messaging'><img src="https://www.dropbox.com/scl/fi/fqpz76nfbr5f6jrynsqkm/Messaging.png?rlkey=76hpdxbpyefilk9qk28lkfklx&raw=1" alt="Messaging" id="messaging"/></Link>
         <Link href='http://localhost:3000/cart'><img src="https://www.dropbox.com/scl/fi/ft27qvt6060xemg8wk4sl/ShoppingCart.png?rlkey=f7yr3x9vgi62p5xerolsbdmg7&raw=1" alt="Cart" id="shoppingCart"/></Link>
         <Link href='http://localhost:3000/sell' id="sell">Sell</Link>
-        <form onSubmit={handleSearch} class="searchBar">
+        <div className="searchBar">
+        <form onSubmit={handleSearch}>
           <input
             type="text"
             name="searchTerm"
@@ -37,6 +38,7 @@ const NavBar = () => {
           />
           <button type="submit" id="searchButton">Submit</button>
         </form>
+        </div>
     </div>
     </>
   )
--- frontend/frontend/src/app/page.js
+++ frontend/frontend/src/app/page.js
@@ -1,18 +1,13 @@
 "use client";
-import NavBar from "./components/NavBar";
+import Layout from "./layout";
 import ReactDOM from "react-dom/client"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import index from "./index"
 
 export default function Home() {
   return (
-    <html>
-      <head>
-      </head>
-      <body>
-        <div id="root"></div>
-        <script src="/index.js"></script>
-      </body>
-    </html>
+    <>
+        <Layout />
+        <div><p>this is a test</p></div>
+    </>
   );
 }
