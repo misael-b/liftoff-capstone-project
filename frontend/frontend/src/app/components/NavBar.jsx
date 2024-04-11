4969b868e34bd0b2e98f8ed221f5d2ec0bbefb45
Kyle Banderman <kyle.banderman4675@gmail.com>
2024-04-11 17:30:06 -0500

    finished navbar

--- frontend/frontend/src/app/components/NavBar.jsx
+++ frontend/frontend/src/app/components/NavBar.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Layout from "../layout"
 //all of these links can be changed to whatever we need to later
 const NavBar = () => {
-  const [search, setSearch] = useState({search: ''})
+  const [search, setSearch] = useState({searchTerm: ''})
 
   const handleChange = (event) => {
     const {name, value} = event.target;
@@ -13,8 +13,9 @@ const NavBar = () => {
   };
 
   return (
-    <Layout> 
+    <> 
     <div className="navbar">
+        <h2 id="teamName">teamName.set()</h2>
         <Link href='http://localhost:3000/profile'><img src="https://www.dropbox.com/scl/fi/p3jwfys2i99m6ypluwfww/Profile.png?rlkey=r3lppn8mj0msj5ventzkjya83&raw=1" alt="Profile" id="profile"/></Link>
         <Link href='http://localhost:3000/messaging'><img src="https://www.dropbox.com/scl/fi/fqpz76nfbr5f6jrynsqkm/Messaging.png?rlkey=76hpdxbpyefilk9qk28lkfklx&raw=1" alt="Messaging" id="messaging"/></Link>
         <Link href='http://localhost:3000/cart'><img src="https://www.dropbox.com/scl/fi/ft27qvt6060xemg8wk4sl/ShoppingCart.png?rlkey=f7yr3x9vgi62p5xerolsbdmg7&raw=1" alt="Cart" id="shoppingCart"/></Link>
@@ -24,7 +25,7 @@ const NavBar = () => {
           <input
             type="text"
             name="searchTerm"
-            value={search.search}
+            value={search.searchTerm}
             onChange={handleChange}
             placeholder="Search:"
             id="searchBox"
@@ -33,7 +34,7 @@ const NavBar = () => {
         </form>
         </div>
     </div>
-    </Layout>
+    </>
   )
 }
 
