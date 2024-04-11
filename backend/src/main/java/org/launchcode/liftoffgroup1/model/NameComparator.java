5b23d92efabd68ce1415a14cbbc22b64c2e26373
mbarcenas <misael.barcenas@yahoo.com>
2024-04-11 17:26:28 -0500

    added category and name sorting

--- backend/src/main/java/org/launchcode/liftoffgroup1/model/NameComparator.java
+++ backend/src/main/java/org/launchcode/liftoffgroup1/model/NameComparator.java
@@ -1,2 +1,10 @@
-package org.launchcode.liftoffgroup1.model;public class NameComparator {
+package org.launchcode.liftoffgroup1.model;
+
+import java.util.Comparator;
+
+public class NameComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product o1, Product o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
 }
