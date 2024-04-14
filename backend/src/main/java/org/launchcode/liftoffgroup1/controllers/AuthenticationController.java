7199b68e24a2ee0243901072006f3fbb99a790c0
mbarcenas <misael.barcenas@yahoo.com>
2024-04-13 20:43:15 -0500

    authentication

--- backend/src/main/java/org/launchcode/liftoffgroup1/controllers/AuthenticationController.java
+++ backend/src/main/java/org/launchcode/liftoffgroup1/controllers/AuthenticationController.java
@@ -1,58 +1,46 @@
 package org.launchcode.liftoffgroup1.controllers;
 
-import org.launchcode.liftoffgroup1.model.data.UserRepository;
-import org.launchcode.liftoffgroup1.model.dto.AuthResponseDTO;
 import org.launchcode.liftoffgroup1.model.dto.LoginDTO;
-import org.launchcode.liftoffgroup1.security.JwtTokenGenerator;
+import org.launchcode.liftoffgroup1.service.TokenService;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
-@RequestMapping("login")
+//@RequestMapping("login")
 @CrossOrigin("http://localhost:3000/")
 public class AuthenticationController {
-    private AuthenticationManager authenticationManager;
-    private UserRepository userRepository;
+    private final TokenService tokenService;
+//    private final AuthenticationManager authenticationManager;
 
 
-    private JwtTokenGenerator jwtTokenGenerator;
-
-    @Autowired
-    public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository, JwtTokenGenerator jwtTokenGenerator) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.jwtTokenGenerator = jwtTokenGenerator;
+    public AuthenticationController(TokenService tokenService
+//            , AuthenticationManager authenticationManager
+    ) {
+        this.tokenService = tokenService;
+//        this.authenticationManager = authenticationManager;
     }
 
-  // API:
+    @PostMapping("/token")
+    public String Token(Authentication authentication){
+        String token = tokenService.generateToken(authentication);
+        return token;
+    }
 
-
-
-
-   @PostMapping("login")
-    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO loginDTO){
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
-       SecurityContextHolder.getContext().setAuthentication(authentication);
-       String token = jwtTokenGenerator.generateToken(authentication);
-       return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.OK);
-   }
-
-//    @PostMapping("login")
-//    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO){
-//        Authentication authentication = authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
-//       SecurityContextHolder.getContext().setAuthentication(authentication);
-//       String token = jwtTokenGenerator.generateToken(authentication);
-//       return new ResponseEntity<>("login successful", HttpStatus.OK);}
-
+    @PostMapping("/login")
+    public String login(@RequestBody LoginDTO loginRequest) {
+        Authentication authenticationRequest =
+                UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.getUsername(), loginRequest.getPassword());
+//        Authentication authenticationResponse =
+//                this.authenticationManager.authenticate(authenticationRequest);
+//        String token = tokenService.generateToken(authenticationResponse);
+//        return token;
+        return "LOGGED IN";
+    }
 }
--- backend/src/main/java/org/launchcode/liftoffgroup1/security/SecurityConfig.java
+++ backend/src/main/java/org/launchcode/liftoffgroup1/security/SecurityConfig.java
@@ -1,57 +1,99 @@
 package org.launchcode.liftoffgroup1.security;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+//
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private JwtAuthEntryPoint authEntryPoint;
-    private CustomUserDetailsService userDetailsService;
+    private final RsaKeyProperties rsaKeys;
 
     @Autowired
-    public SecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthEntryPoint authEntryPoint) {
-        this.userDetailsService = userDetailsService;
-        this.authEntryPoint = authEntryPoint;
+    public SecurityConfig(RsaKeyProperties rsaKeys) {
+        this.rsaKeys = rsaKeys;
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable)
+    public UserDetailsService users() {
+        User.UserBuilder users = User.builder();
+        UserDetails user = users
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = users
+                .username("admin")
+                .password("password")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 
+    //
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
-//                .requestMatchers("/", "/list/**", "/api/auth/**", "/search", "/api/auth/login").permitAll()
-//                .anyRequest().authenticated()
-                                .anyRequest().permitAll()
-        ).exceptionHandling((exception)-> exception.
-                        authenticationEntryPoint(authEntryPoint))
-
-                .formLogin((form) -> form
-                        .loginPage("/user/login")
-                        .permitAll()
-                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-        return http.build();
-
+                .requestMatchers("/", "/list", "/login", "/search").permitAll()
+                .anyRequest().authenticated()
+//                        .anyRequest().permitAll()
+                )
+//                .oauth2ResourceServer((OAuth2ResourceServer)-> OAuth2ResourceServer.jwt((jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .build();
     }
 
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//            UserDetailsService userDetailsService,
+//            PasswordEncoder passwordEncoder) {
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService(userDetailsService);
+//        authenticationProvider.setPasswordEncoder(passwordEncoder);
+//
+//        return new ProviderManager(authenticationProvider);
+//    }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
-        return authenticationConfiguration.getAuthenticationManager();
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 
     @Bean
@@ -59,8 +101,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter(){
-        return new JwtAuthenticationFilter();
-    }
 }
