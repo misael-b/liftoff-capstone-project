184c4ae38bf7aa2618d492ff48b2699ac81830f4
mbarcenas <misael.barcenas@yahoo.com>
2024-04-18 22:06:42 -0500

    logout of profile page

--- frontend/frontend/src/app/profile/page.jsx
+++ frontend/frontend/src/app/profile/page.jsx
@@ -2,37 +2,62 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Layout from '../layout'
+import { homePage } from "../actions";
+
+async function handleLogout(event) {
+  event.preventDefault();
+  try {
+    const token = JSON.parse(localStorage.getItem('user')).accessToken
+    const AuthStr = 'Bearer '.concat(token);
+    window.localStorage.removeItem('user')
+    console.log(JSON.parse(localStorage.getItem('user')).accessToken)
+  } catch (e) {
+    console.log("Sign to logout", e);
+  }
+  finally {
+    homePage()
+  }
+}
+
 
 
 const page = () => {
   const [username, setUsername] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  let user;
-  useEffect(() => {
-      const token = JSON.parse(localStorage.getItem('user')).accessToken
-    const AuthStr = 'Bearer '.concat(token);
-    axios.get(
-        'http://localhost:8080/login/user',
-        {
-          headers: {
-            accept: "*/*",
-            "Content-Type": 'application/json',
-            Authorization: AuthStr
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem('user') !== null) {
+      let user;
+      useEffect(() => {
+        const token = JSON.parse(localStorage.getItem('user')).accessToken
+        const AuthStr = 'Bearer '.concat(token);
+        axios.get(
+          'http://localhost:8080/login/user',
+          {
+            headers: {
+              accept: "*/*",
+              "Content-Type": 'application/json',
+              Authorization: AuthStr
+            }
           }
-        }
-      ).then((res) => {
-        user = res.data
-        setUsername(user.username)
-        setName(user.name)
-        setEmail(user.email)
-        
-        
-      })
-  }, []);
+        ).then((res) => {
+          user = res.data
+          setUsername(user.username)
+          setName(user.name)
+          setEmail(user.email)
+
+
+        })
+      }, []);
+    } else {
+      homePage()
+    }
+  }
+  
 
   return (<Layout>
-    <p style={{color: "black"}}>Welcome {username}</p>
+    <p style={{ color: "black", margin: 70 }}>Welcome {username}</p>
+    <button onClick={handleLogout}>Logout</button>
   </Layout>
 
   )
